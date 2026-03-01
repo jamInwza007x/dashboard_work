210952b5c0029b3103343978608b08360854d5dc
Jiratada <6810110448@psu.ac.th>
2026-03-01 19:19:50 +0700

    12: Configure slider range

--- app.py
+++ app.py
@@ -7,4 +7,10 @@ df = px.data.gapminder()
 app = dash.Dash(__name__)
 
 app.layout = html.Div([html.H1("AI Engineering Dashboard"),
-                       dcc.Slider(id='year-slider'),])
+                        dcc.Slider(
+                        id='year-slider',
+                        min=df['year'].min(),
+                        max=df['year'].max(),
+                        value=df['year'].min(),
+                        marks={str(y): str(y) for y in df['year'].unique()}
+    ),
