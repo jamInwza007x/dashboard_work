8bb67b114e58b1bbfe59b04bab5b36e75c29aaee
Jiratada <6810110448@psu.ac.th>
2026-03-01 19:11:18 +0700

    9: Import dash components

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import dash
+from dash import dcc, html, Input, Output
 import pandas as pd
 import plotly.express as px
 
 df = px.data.gapminder()
 app = dash.Dash(__name__)
+
+app.layout = html.Div([]
